5c5aa428ca742f67631c5c4bcc8ae29c6d3dcfdd
Mat Chan <mat.chan@ibm.com>
2022-03-02 21:04:02 +0800

    update: enable useMousePosition
    - and add description on how we achieve performance optimization

--- src/hooks/useMouse.js
+++ src/hooks/useMouse.js
@@ -7,10 +7,15 @@ const useMousePosition = () => {
     setMousePosition({ x: ev.clientX, y: ev.clientY });
   }, 100);
 
+  // Try to use the method below and hover on revenue, it will be so laggy
+  // const updateMousePosition = (ev) => {
+  //   setMousePosition({ x: ev.clientX, y: ev.clientY });
+  // };
+
   useEffect(() => {
     window.addEventListener("mousemove", updateMousePosition);
     return () => window.removeEventListener("mousemove", updateMousePosition);
-  }, []);
+  }, [updateMousePosition]);
   return mousePosition;
 };
 
